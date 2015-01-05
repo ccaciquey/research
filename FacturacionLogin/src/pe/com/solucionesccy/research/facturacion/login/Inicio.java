364f8a4ad64c389c3d1bc283999e9e156700a357
Carlos Cacique <carlos.cacique@gmail.com>
2015-01-05 10:33:18 -0500

    Realizando el primer commit

---
+++ FacturacionLogin/src/pe/com/solucionesccy/research/facturacion/login/Inicio.java
@@ -0,0 +1,9 @@
+package pe.com.solucionesccy.research.facturacion.login;
+
+public class Inicio {
+
+	public static void main(String[] argumentos){
+		System.out.println("Empezando con pie derecho el 2015...");
+	}
+	
+}
