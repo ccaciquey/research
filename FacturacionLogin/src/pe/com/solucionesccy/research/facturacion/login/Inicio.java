18a3d1294e04efa42b0c4b4363fd357edd6b0483
Carlos Cacique <carlos.cacique@gmail.com>
2015-01-05 10:37:09 -0500

    Cambios y más cambios....

--- FacturacionLogin/src/pe/com/solucionesccy/research/facturacion/login/Inicio.java
+++ FacturacionLogin/src/pe/com/solucionesccy/research/facturacion/login/Inicio.java
@@ -3,7 +3,9 @@ package pe.com.solucionesccy.research.facturacion.login;
 public class Inicio {
 
 	public static void main(String[] argumentos){
-		System.out.println("Empezando con pie derecho el 2015...");
+		System.out.println("Empezando con pie derecho el 2015.....");
+		System.out.println("Empezando con pie derecho el 2015.....");
+		System.out.println("Empezando con pie derecho el 2015.....");
 	}
 	
 }
